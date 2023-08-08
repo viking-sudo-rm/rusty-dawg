15bd56dd76130eda9251cedd2810edff22cb51a6
Will Merrill <vikingarnir.will@gmail.com>
2023-08-07 23:02:25 -0700

    Update README and benchmarking script

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 
+DATA_PATH="${DATA:-data}"
+
 ./target/release/rusty-dawg \
-    --train-path data/$1/wiki.train.raw \
-    --test-path data/$1/wiki.valid.raw \
+    --train-path $DATA_PATH/$1/wiki.train.raw \
+    --test-path $DATA_PATH/$1/wiki.valid.raw \
     --save-path "/tmp/$1.dawg" \
     --results-path "" \
     --n-eval 0 \
