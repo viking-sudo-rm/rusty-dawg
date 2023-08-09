f4d51af8cf6218fe0c6cab77f03c28362abbd292
epwalsh <petew@allenai.org>
2023-08-09 13:32:09 -0700

    ensure Python bindings version updated on release

--- scripts/release.sh
+++ scripts/release.sh
@@ -19,13 +19,14 @@ tag="v${version}"
 read -p "Creating new release for $tag. Do you want to continue? [Y/n] " prompt
 
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
-    echo "Updating Cargo.toml and CHANGELOG for release..."
+    echo "Updating Cargo.toml files and CHANGELOG for release..."
 else
     echo "Cancelled"
     exit 1
 fi
 
 toml_set Cargo.toml package.version "$version"
+toml_set bindings/python/Cargo.toml package.version "$version"
 VERSION="$version" python scripts/prepare_changelog.py
 
 read -p "Updated files, please check for errors. Do you want to continue? [Y/n] " prompt
