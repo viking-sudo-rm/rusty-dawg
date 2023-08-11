a21f3d2e69a53165e41d75d1e85dd50fe61c434f
Will Merrill <vikingarnir.will@gmail.com>
2023-08-11 12:33:18 -0700

    Configure run_local.sh

--- scripts/run_local.sh
+++ scripts/run_local.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/bash
 
 ./target/release/rusty-dawg \
-    --train-path data/wikitext-2-raw/wiki.train.raw \
-    --test-path data/wikitext-2-raw/wiki.valid.raw \
-    --save-path data/wikitext2_origin.dawg \
-    --results-path data/wikitext2.json \
+    --train-path data/$1/wiki.train.raw \
+    --test-path data/$1/wiki.valid.raw \
+    --save-path data/$1.dawg \
+    --results-path data/$1.json \
     --n-eval 0 \
     --nodes-ratio 1.25 \
     --edges-ratio 2.20 \
-    --tokenizer gpt2
+    --tokenizer gpt2 \
+    --utype u16
     # -f 0 -f 1024 -f 2048 -f 4096 -f 8192 -f 16384 \
     # -d 0.01 -d 0.05 -d 0.1 -d 0.3 -d 0.5 \
     # -n 4 \
