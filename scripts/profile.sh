5d3677bd457164d9358a8e6b1db2e0382bd7e935
Will Merrill <vikingarnir.will@gmail.com>
2023-08-10 19:12:25 -0700

    Add profiling script

---
+++ scripts/profile.sh
@@ -0,0 +1,1 @@
+flamegraph --root -o flame.svg -- ./target/release/rusty-dawg --train-path "data/wikitext-small/wiki.train.raw" --test-path "data/wikitext-small/wiki.valid.raw" --save-path "" --results-path "" --n-eval 0 --tokenize
