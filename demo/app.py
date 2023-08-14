dcda8a25db9029cacb68c01336b8cead657cba39
David Wadden <davidw@allennlp-cirrascale-51.reviz.ai2.in>
2023-08-14 22:22:25 +0000

    Move app info (examples, etc) into separate file.

--- demo/app.py
+++ demo/app.py
@@ -1,31 +1,12 @@
 from app_helpers import run_query
+import app_info
 import gradio as gr
 
 
-description = """# Rusty Dawg Visualizer
+# Launch the app.
 
-Rusty DAWG enables very fast n-gram search over a large corpus, such as those used to
-train LLMs. Given a query string as input, Rusty DAWG will search the for spans that
-appear in both the corpus and the query string. It will render the matching spans,
-indicating how often each one appears in the training data. Each span will be annotated
-as `{span id}|{span length in tokens}|{span count in corpus}`."""
-
-wikitext_example = """In January 1844 , North Carolina Representative James Iver McKay ,
-the chairman of the Committee on Ways and Means , solicited the views of Director
-Patterson on the gold dollar
-"""
-
-mj_example = """Michael Jeffrey Jordan (born February 17, 1963), also known by his
-initials MJ,[9] is an American former professional basketball player and businessman.
-The official National Basketball Association (NBA) website states: "By acclamation,
-Michael Jordan is the greatest basketball player of all time."
-"""
-
-examples = [example.replace("\n", " ") for example in [wikitext_example, mj_example]]
-span_css = ".spans { font-size: 16px; }"
-
-with gr.Blocks(css=span_css) as demo:
-    title = gr.Markdown(value=description)
+with gr.Blocks(css=app_info.span_css) as demo:
+    title = gr.Markdown(value=app_info.description)
     with gr.Row(variant="panel"):
         with gr.Column(scale=1, min_width=400):
             query = gr.Textbox(label="Query")
@@ -37,7 +18,7 @@ with gr.Blocks(css=span_css) as demo:
                         1, 5, value=4, step=1, label="Min n-gram display length"
                     )
         with gr.Column(scale=1, min_width=400):
-            examples = gr.Examples(examples=examples, inputs=query)
+            examples = gr.Examples(examples=app_info.examples, inputs=query)
     with gr.Row(variant="panel"):
         html = gr.HTML()
 
