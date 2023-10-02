2e4bb28e33f12b26ddf7fbd87911dcad6f137e91
Will Merrill <willm@allenai.org>
2023-10-02 12:14:44 -0400

    Comment out DiskDawg from Python wrapper

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -1,19 +1,16 @@
 use pyo3::prelude::*;
-use pyo3::wrap_pyfunction;
 
 pub mod dawg;
-pub mod disk_dawg;
+// pub mod disk_dawg;  // FIXME(#53): Vector type used by disk-backed DAWG is not thread-safe.
 
 use dawg::Dawg;
-use disk_dawg::DiskDawg;
+// use disk_dawg::DiskDawg;
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn rusty_dawg(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Dawg>()?;
-    m.add_class::<DiskDawg>()?;
-    // Here's how you would add a function:
-    // m.add_wrapped(wrap_pyfunction!(good_turing_estimate))?;
+    // m.add_class::<DiskDawg>()?;
     Ok(())
 }
 
