03f84b9dc28e48ec913f79a5873e444a315fac5b
Will Merrill <willm@allenai.org>
2024-02-15 16:16:47 -0500

    CachedDiskVec supports empty cache

--- src/memory_backing/vec_backing/cached_disk_vec.rs
+++ src/memory_backing/vec_backing/cached_disk_vec.rs
@@ -15,7 +15,7 @@ where
     Ix: IndexType,
 {
     vec: DiskVec<T>,
-    cache: LruCache<Ix, T>,
+    cache: Option<LruCache<Ix, T>>,
 }
 
 impl<T, Ix> CachedDiskVec<T, Ix>
@@ -32,14 +32,22 @@ where
         cache_size: usize,
     ) -> Result<Self> {
         let vec = DiskVec::new(path, capacity)?;
-        let cache = LruCache::new(NonZeroUsize::new(cache_size).unwrap());
+        let cache = if cache_size > 0 {
+            Some(LruCache::new(NonZeroUsize::new(cache_size).unwrap()))
+        } else {
+            None
+        };
         Ok(Self { vec, cache })
     }
 
     /// Load a read-only `DiskVec<T>` from an existing file.
     pub fn load<P: AsRef<Path> + std::fmt::Debug>(path: P, cache_size: usize) -> Result<Self> {
         let vec = DiskVec::load(path)?;
-        let cache = LruCache::new(NonZeroUsize::new(cache_size).unwrap());
+        let cache = if cache_size > 0 {
+            Some(LruCache::new(NonZeroUsize::new(cache_size).unwrap()))
+        } else {
+            None
+        };
         Ok(Self { vec, cache })
     }
 
@@ -50,15 +58,19 @@ where
         cache_size: usize,
     ) -> Result<Self> {
         let vec = DiskVec::from_vec(vec, path)?;
-        let cache = LruCache::new(NonZeroUsize::new(cache_size).unwrap());
+        let cache = if cache_size > 0 {
+            Some(LruCache::new(NonZeroUsize::new(cache_size).unwrap()))
+        } else {
+            None
+        };
         Ok(Self { vec, cache })
     }
 
-    /// Convert a writable `DiskVec<T>` into a read-only `DiskVec<T>`.
-    pub fn make_read_only(mut self) -> Result<()> {
-        let _ = self.vec.make_read_only()?;
-        Ok(())
-    }
+    // /// Convert a writable `DiskVec<T>` into a read-only `DiskVec<T>`.
+    // pub fn make_read_only(mut self) -> Result<()> {
+    //     let _ = self.vec.make_read_only()?;
+    //     Ok(())
+    // }
 
     pub fn try_reserve(&mut self, additional: usize) -> Result<()> {
         self.vec.try_reserve(additional)
@@ -70,12 +82,19 @@ where
         Ok(())
     }
 
+    fn pop_cache(&mut self, index: usize) {
+        // See https://users.rust-lang.org/t/if-let-passing-a-mutable-ref-option-not-working/31300
+        if let Some(cache) = self.cache.as_mut() {
+            cache.pop(&Ix::new(index));
+        }
+    }
+
     /// A hacky way to use the DiskVec as a stack.
     /// Possible strange interactions with other methods that use len!!
     pub fn pop(&mut self) -> Result<Option<T>> {
         let value = self.vec.pop()?;
         if value.is_some() {
-            self.cache.pop(&Ix::new(self.vec.len()));
+            self.pop_cache(self.vec.len());
         }
         Ok(value)
     }
@@ -83,7 +102,7 @@ where
     /// Set the item at the given index. Removes that item from the cache.
     pub fn set(&mut self, index: usize, value: &T) -> Result<()> {
         self.vec.set(index, value)?;
-        self.cache.pop(&Ix::new(index));
+        self.pop_cache(index);
         Ok(())
     }
 
@@ -100,13 +119,17 @@ where
     /// Get the item at the given index.
     pub fn get(&mut self, index: usize) -> Result<T> {
         let idx = Ix::new(index);
-        match self.cache.get(&idx) {
-            Some(value) => Ok(*value),
-            None => {
-                let value = self.vec.get(index)?;
-                self.cache.put(idx, value);
-                Ok(value)
+        if let Some(cache) = self.cache.as_mut() {
+            match cache.get(&idx) {
+                Some(value) => Ok(*value),
+                None => {
+                    let value = self.vec.get(index)?;
+                    cache.put(idx, value);
+                    Ok(value)
+                }
             }
+        } else {
+            self.vec.get(index)
         }
     }
 }
@@ -117,7 +140,7 @@ mod tests {
     use tempfile::tempdir;
 
     #[test]
-    fn test_push_integers() {
+    fn test_push_get_integers() {
         let tmp_dir = tempdir().unwrap();
         let capacity = 10;
         let cache_size = 5;
@@ -130,6 +153,11 @@ mod tests {
             let _ = vec.push(&value);
         }
 
+        // Before "getting", cache is fully empty.
+        for idx in 0..10 {
+            assert_eq!(vec.cache.as_mut().unwrap().get(&DefaultIx::new(idx)), None);
+        }
+
         // Check that "get" returns the right values.
         for idx in 0..10 {
             assert_eq!(vec.get(idx).unwrap(), idx + 10);
@@ -137,11 +165,54 @@ mod tests {
 
         // Only the last five things should fit in the cache.
         for idx in 0..5 {
-            assert_eq!(vec.cache.get(&DefaultIx::new(idx)), None);
+            assert_eq!(vec.cache.as_mut().unwrap().get(&DefaultIx::new(idx)), None);
         }
         for idx in 5..10 {
             let value = idx + 10;
-            assert_eq!(vec.cache.get(&DefaultIx::new(idx)), Some(&value));
+            assert_eq!(
+                vec.cache.as_mut().unwrap().get(&DefaultIx::new(idx)),
+                Some(&value)
+            );
         }
+    }
+
+    #[test]
+    fn test_push_set_get_integers() {
+        let tmp_dir = tempdir().unwrap();
+        let capacity = 10;
+        let cache_size = 5;
+        let mut vec: CachedDiskVec<usize> =
+            CachedDiskVec::new(tmp_dir.path().join("vec.bin"), capacity, cache_size).unwrap();
+        assert_eq!(vec.len(), 0);
+
+        for idx in 0..10 {
+            let value = idx + 10;
+            let _ = vec.push(&value);
+        }
+
+        for idx in 0..5 {
+            let value = idx + 20;
+            let _ = vec.set(idx, &value);
+        }
+
+        for idx in 0..10 {
+            assert_eq!(vec.cache.as_mut().unwrap().get(&DefaultIx::new(idx)), None);
+        }
+
+        for idx in 0..5 {
+            assert_eq!(vec.get(idx).unwrap(), idx + 20);
+        }
+        for idx in 5..10 {
+            assert_eq!(vec.get(idx).unwrap(), idx + 10);
+        }
+    }
+
+    #[test]
+    fn test_empty_cache() {
+        let tmp_dir = tempdir().unwrap();
+        let capacity = 10;
+        let vec: CachedDiskVec<usize> =
+            CachedDiskVec::new(tmp_dir.path().join("vec.bin"), capacity, 0).unwrap();
+        assert!(vec.cache.is_none());
     }
 }
