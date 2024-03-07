20fc3274a4d6bf2ee9d98966d95be857a89dbf34
Will Merrill <willm@allenai.org>
2024-03-07 00:32:16 -0500

    Update tokenizer to pythia-12b

--- scripts/cdawg/run_pile.sh
+++ scripts/cdawg/run_pile.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 # Comment out last two flags to run in RAM.
+# Some tokenizer options: EleutherAI/pythia-12b, allenai/OLMo-7B
 
 DATA_PATH=${1:-"/net/nfs.cirrascale/allennlp/willm/data/pile/00_0.json.gz"}
 RUN_DIR=${2:-"/home/willm/pile-run"}
@@ -15,7 +16,7 @@ RUST_BACKTRACE=full ./target/release/rusty-dawg \
     --edges-ratio $EDGES_RATIO \
     --cache-size 0 \
     --buf-size 3000000000 \
-    --tokenizer "allenai/OLMo-7B" \
+    --tokenizer "EleutherAI/pythia-12b" \
     --data-reader "pile" \
     --utype u16 \
     --cdawg \
