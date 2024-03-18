d32e320779874f0406c1ee87c48e125d1ab3eba2
Will Merrill <vikingarnir.will@gmail.com>
2024-03-18 10:09:36 -0700

    Update setup.sh script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
 # Setup a new VM to run Rusty DAWG
 
-DATA_PATH=$1
-
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # TODO: Probably need to rerun setup script or something here
 cargo build --release
 
+DATA_PATH=$1
+LOCAL_DIR=/home/willm/data
+
 # Login to Google Cloud and download the data
 gcloud auth login
-gcloud compute scp --zone "us-central1-a" "lm-datasets:/mnt/tank/pile/train/$DATA_PATH" data.jsonl.gz
+gcloud compute scp --zone "us-central1-a" "lm-datasets:/mnt/tank/pile/train/$DATA_PATH" $LOCAL_DIR/data.jsonl.gz
