60d65ed702cc21b8f42942462cd71ea09ed0eaf4
Will Merrill <willm@allenai.org>
2024-04-03 21:15:35 -0400

    Clean up scripts

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,3 +1,5 @@
+# Examples of how to profile with flame graph.
+
 CORPUS=wikitext-2000
 
 flamegraph --root -o flame.svg -- ./target/release/rusty-dawg --train-path "data/$CORPUS/wiki.train.raw" --test-path "data/$CORPUS/wiki.valid.raw" --save-path "" --results-path "" --n-eval 0 --tokenize
