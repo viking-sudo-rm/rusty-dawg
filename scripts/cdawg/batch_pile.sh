13d46cbdac65d833d05a7bed2b27250426eedab4
Will Merrill <vikingarnir.will@gmail.com>
2024-04-07 14:53:23 -0700

    Update scripts

--- scripts/cdawg/batch_pile.sh
+++ scripts/cdawg/batch_pile.sh
@@ -4,18 +4,19 @@
 START=$1
 END=$2
 PREFIX=willm-pile2
-echo "=== Running from $START to $END ==="
+echo "Running from $START to $END..."
 
 for split in {$START..$END}; do
     MACHINE=$PREFIX-$split
-    echo "Creating $MACHINE..."
+    echo "=== Creating $MACHINE... ==="
     gcloud compute instances create $MACHINE --source-instance-template willm-ram-384gb --zone "us-central1-a"
 done
 
 for split in {$START..$END}; do
     MACHINE=$PREFIX-$split
+    echo "=== Launching $MACHINE... ==="
     gcloud compute scp --zone "us-central1-a" --recurse /home/willm/rusty-dawg-startup $MACHINE:/home/willm/rusty-dawg-startup
-    cmd="screen -dmS cdawg bash -c 'export SPLIT=$split && chmod +x rusty-dawg-startup/wrap_startup.sh && rusty-dawg-startup/wrap_startup.sh; exec bash'"
+    cmd="screen -dmS cdawg bash -c 'export SPLIT=$split && chmod +x rusty-dawg-startup/wrap_startup.sh && rusty-dawg-startup/wrap_startup.sh && sudo shutdown -h now; exec bash'"
     echo "Running command: $cmd"
     gcloud compute ssh --zone "us-central1-a" willm@$MACHINE --command="$cmd"
 done
